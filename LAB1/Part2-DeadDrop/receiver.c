cd9e178697349c500106b6931fa62ae2b5cc2d4f
p2agrawal <p2agrawal@ucsd.edu>
2022-10-20 17:19:25 -0700

    Part1 Complete

--- LAB1/Part2-DeadDrop/receiver.c
+++ LAB1/Part2-DeadDrop/receiver.c
@@ -2,27 +2,106 @@
 #include"util.h"
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
+#include <time.h>
+#define SAMPLES 10
+ 
+uint64_t get_cache_set_index(ADDR_PTR phys_addr){
+	uint64_t mask = ((uint64_t) 1 << 15) - 1;
+	return (phys_addr & mask) >> 6;
+}
 
 int main(int argc, char **argv)
 {
 	// Put your covert channel setup code here
+int set_id[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 
-	printf("Please press enter.\n");
+ uint64_t eviction_set[8][8];
 
-	char text_buf[2];
-	fgets(text_buf, sizeof(text_buf), stdin);
+ volatile char tmp; 
 
-	printf("Receiver now listening.\n");
+ int n = 8;
+ int miss_threshold = 70;
 
-	bool listening = true;
-	while (listening) {
+ int o = 6;                          // log_2(64), where 64 is the line size
+ int s = 14;                         // log_2(16384), where 16384 is the number of cache sets
+ int two_o = 64;             // 64
+ int two_o_s = 512;   // 1048576
+ int b = 262144;
 
-		// Put your covert channel code here
+ long int misses[8][8];
+ long int hits[8][8];
 
-	}
+ char *buffer = malloc((size_t) b);
 
-	printf("Receiver finished.\n");
+ uint64_t addr;
 
+ clock_t current_time, start_time;
+
+  for (int ind = 0; ind <8; ind++) {
+	  for (int way = 0; way <8; way++) {
+		  misses[ind][way] = 0;
+	  }
+  }
+
+  for (int ind = 0; ind <8; ind++) {
+	  for (int way = 0; way <8; way++) {
+		  hits[ind][way] = 0;
+	  }
+  }
+
+
+ for (int set_id_index = 0; set_id_index < 8; set_id_index++) {
+     int address_count = 0;
+     for (int set_index = 0; set_index < 512; set_index++) {
+       for (int way_index = 0; way_index < 8; way_index++) {
+	  addr = (uint64_t) (buffer + set_index * two_o_s + way_index * two_o);
+           if (get_cache_set_index(addr) == set_id[set_id_index]) {
+		 eviction_set[set_id_index][address_count] = addr;
+		 address_count++;
+	   }
+         }	 
+       }
+     }
+
+
+ for (int repeat=0; repeat < 100000; repeat++) {
+	for (int ind = 0; ind < 8; ind++) {
+          for (int way = 0; way < 8; way++){
+              uint64_t *temp_pointer = (uint64_t *) eviction_set[ind][way];
+	      tmp = *temp_pointer;
+	    }
+   	}
+    }
+
+ start_time = clock();
+ current_time = start_time;
+
+while (current_time - start_time < 10000000) {
+current_time = clock();
+
+}	
+printf("Prime is done\n");       
+
+        bool listening = true;
+          for (int ind = 0; ind < 8; ind++) {
+	    for (int way = 0; way < 8; way++){
+         start_time = clock();
+         current_time = start_time;
+	    while (listening && current_time - start_time < 200) {
+            int time = measure_one_block_access_time(eviction_set[ind][way]);
+	    
+	   if (time < 700) {
+
+            if (time > miss_threshold)
+	     printf("For set %d we have miss\n", ind);		    
+	    else
+		    ;
+           }		    
+  current_time = clock();      
+       }
+    }
+
+}
 	return 0;
 }
 
--- LAB1/Part2-DeadDrop/sender.c
+++ LAB1/Part2-DeadDrop/sender.c
@@ -2,26 +2,61 @@
 #include"util.h"
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
-
+#include <math.h>
+#include <time.h>
 // TODO: define your own buffer size
 #define BUFF_SIZE (1<<21)
 //#define BUFF_SIZE [TODO]
+uint64_t get_cache_set_index(ADDR_PTR phys_addr){
+	uint64_t mask = ((uint64_t) 1 << 15) - 1;
+	return (phys_addr & mask) >> 6;
+}
+
 
 int main(int argc, char **argv)
 {
+	int set_id[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+	uint64_t eviction_set[8][8];
+ 	volatile char tmp;
   // Allocate a buffer using huge page
   // See the handout for details about hugepage management
-  void *buf= mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE, MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB, -1, 0);
-  
+  	void *buf= mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE, MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB, -1, 0);
+  	int n = 8;
   if (buf == (void*) - 1) {
      perror("mmap() error\n");
      exit(EXIT_FAILURE);
   }
+
+ 	int o = 6;                          // log_2(64), where 64 is the line size
+ 	int s = 14;                         // log_2(16384), where 16384 is the number of cache sets
+ 	int two_o = 64;             // 64
+ 	int two_o_s = 512;   // 1048576
+ 	int b = 262144;
+
+	char *buffer = malloc((size_t) b);
+
+ 	uint64_t addr;
+
+ 	clock_t current_time, start_time;
+
+ 	for (int set_id_index = 0; set_id_index < 8; set_id_index++) {
+     		int address_count = 0;
+     		for (int set_index = 0; set_index < 512; set_index++) {
+       			for (int way_index = 0; way_index < 8; way_index++) {
+	  			addr = (uint64_t) (buffer + set_index * two_o_s + way_index * two_o);
+           			if (get_cache_set_index(addr) == set_id[set_id_index]) {
+		 			eviction_set[set_id_index][address_count] = addr;
+		 			address_count++;
+	   }
+         }
+       }
+     }
+
   // The first access to a page triggers overhead associated with
   // page allocation, TLB insertion, etc.
   // Thus, we use a dummy write here to trigger page allocation
   // so later access will not suffer from such overhead.
-  //*((char *)buf) = 1; // dummy write to trigger page allocation
+  *((char *)buf) = 1; // dummy write to trigger page allocation
 
 
   // TODO:
@@ -30,16 +65,40 @@ int main(int argc, char **argv)
   printf("Please type a message.\n");
 
   bool sending = true;
+  uint64_t *temp_pointer;
+  uint64_t tmp_point;
   while (sending) {
       char text_buf[128];
       fgets(text_buf, sizeof(text_buf), stdin);
-
-      // TODO:
-      // Put your covert channel code here
+      int msg = string_to_int(text_buf);
+      int msg_bin[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+      int i = 0;
+  for ( ;msg > 0; ){
+    msg_bin[i++] = msg % 2;
+    msg /= 2;
   }
+    size_t msg_len = 8;
+    for (int ind = 0; ind < msg_len; ind++) {
+       if (msg_bin[ind] == 0) {
+          ;//No access
+      }
+    else {
+   for (int way = 0; way < 8; way++){
+    start_time = clock();
+    current_time = start_time;
+     while (current_time - start_time < 250000) { 
+	clflush(eviction_set[ind][way]);
+   	current_time = clock();
+         }
+       }
+    
+     }
+   } 
+    printf("Sender Access End\n");
 
-  printf("Sender finished.\n");
-  return 0;
+   }
+
+ return 0;
+
 }
-
-
+   
